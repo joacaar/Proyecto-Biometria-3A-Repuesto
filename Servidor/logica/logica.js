6ae3ccd7dd5976db80d9f5b7f8bf31fab13e204a
EMILIO <emilioesteve99@icloud.com>
2019-10-28 18:01:18 +0100

    Update logica.js

--- Servidor/logica/logica.js
+++ Servidor/logica/logica.js
@@ -177,6 +177,8 @@ async darAltaUsuario( datos ){
 
   var res = await this.getUltimoIDUsuario();
 
+  var res = await this.buscarUsuarioPorEmail(datos.email)
+
   // encriptamos la contraseña con el email.
   var laPasswordEncriptada = sjcl.encrypt(datos.email, datos.password)
 
@@ -185,7 +187,15 @@ async darAltaUsuario( datos ){
   }
   return new Promise( ( resolver, rechazar ) => {
     this.laConexion.run( textoSQL, valoresParaSQL, function( err ) {
-            ( err ? rechazar( err ) : resolver( ) )
+        if( err ){
+            rechazar(err)
+        }
+        if( res == undefined ){
+          resolver()
+        }
+        else{
+          resolver("Ya existe")
+        }
     })
   })
 }
@@ -280,7 +290,7 @@ async iniciarSesion(datos){
         resolver(true)
       }
     } catch (error) {
-      rechazar(false)
+      resolver(false)
     }
 
   })
